b4640cc1b5106e1545e58f270a0417fe56ae6d05
The Hoang <phanthehoang2503@gmail.com>
2025-08-19 16:11:00 +0700

    Initial commit: Expense

---
+++ src/main/java/com/HoangThe/TTCLI/Expense.java
@@ -0,0 +1,32 @@
+package com.HoangThe.TTCLI;
+
+import java.time.LocalDate;
+
+public class Expense {
+    private String description;
+    private double amount;
+    private LocalDate date;
+
+    public Expense(String description, double amount, LocalDate date) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return date + " | " + description + " | " + amount + " VND";
+    }
+}
